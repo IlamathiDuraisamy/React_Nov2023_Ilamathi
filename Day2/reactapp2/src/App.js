c614881e427f747655378b5e436cebcb65a696a5
IlamathiDuraisamy <124855748+IlamathiDuraisamy@users.noreply.github.com>
2023-11-16 12:36:17 +0530

    Day2

---
+++ Day2/reactapp2/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import InlineStyle from "./components/InlineStyle";
+function App()
+{
+  return(
+    <div className="App">
+      <InlineStyle/>
+    </div>
+  );
+}
+export default App;
